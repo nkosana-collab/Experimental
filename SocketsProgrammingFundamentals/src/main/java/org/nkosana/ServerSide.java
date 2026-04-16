7735af5dd051dee3b01c7e686055dacb1a9f7a24
= <=>
2026-04-16 14:12:14 +0200

    Feat: Implement basic ServerSide

--- SocketsProgrammingFundamentals/src/main/java/org/nkosana/ServerSide.java
+++ SocketsProgrammingFundamentals/src/main/java/org/nkosana/ServerSide.java
@@ -1,8 +1,29 @@
 package org.nkosana;
 
-import java.io.*;
-import java.net.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class ServerSide {
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+
+        // STEP 1: Open the port.
+        try {
+            ServerSocket serverSocket = new ServerSocket(5000);
+
+            // STEP 2: Wait for a visitor.
+            Socket clientSocket = serverSocket.accept();
+
+            // STEP 3: Open the "mouth" (Output)
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(),true);
+
+            // STEP 4: Open the "Ear" (Output)
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
